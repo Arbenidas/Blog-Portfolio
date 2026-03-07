7e557399d7ab651414ad052175f388f193e184ef
Diego Clemente <arbe@Diegos-MacBook-Air.local>
2026-03-07 00:29:57 -0600

    fix: import prismjs change to APP_INITIALIZZER

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,11 +1,11 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  { path: 'works/:slug', renderMode: RenderMode.Server },
-  { path: 'logs/:slug', renderMode: RenderMode.Server },
-  { path: 'guides/:slug', renderMode: RenderMode.Server },
+  { path: 'works/:slug', renderMode: RenderMode.Client },
+  { path: 'logs/:slug', renderMode: RenderMode.Client },
+  { path: 'guides/:slug', renderMode: RenderMode.Client },
   { path: 'admin/editor/:id', renderMode: RenderMode.Client },
-  { path: 'u/:username', renderMode: RenderMode.Server },
+  { path: 'u/:username', renderMode: RenderMode.Client },
   {
     path: '**',
     renderMode: RenderMode.Prerender
